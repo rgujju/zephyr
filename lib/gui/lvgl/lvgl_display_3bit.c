87f87f7a7a6a1f2a49c342e52efb233a8e6aff08
Rohit Gujarathi <gujju.rohit@gmail.com>
2021-02-26 23:21:02 +0530

    ratch: lvgl: Updated support for 3 bit displays like lpm013m126c

--- lib/gui/lvgl/lvgl_display_3bit.c
+++ lib/gui/lvgl/lvgl_display_3bit.c
@@ -9,15 +9,15 @@
 #include "lvgl_display.h"
 
 void lvgl_flush_cb_3bit(struct _disp_drv_t *disp_drv,
-		const lv_area_t *area, lv_color_t *color_p)
+			const lv_area_t *area, lv_color_t *color_p)
 {
 	struct device *display_dev = (struct device *)disp_drv->user_data;
 	uint16_t w = area->x2 - area->x1 + 1;
 	uint16_t h = area->y2 - area->y1 + 1;
 	struct display_buffer_descriptor desc;
 
-	/* INFO: Formula hardcoded for LPM0XX */
-	desc.buf_size = ((w >> 1)+2) * h;
+	/* INFO: RATCH: Formula hardcoded for LPM0XX */
+	desc.buf_size = ((w >> 1) + 2) * h;
 	desc.width = w;
 	desc.pitch = w;
 	desc.height = h;
@@ -27,20 +27,20 @@ void lvgl_flush_cb_3bit(struct _disp_drv_t *disp_drv,
 }
 
 #if defined(__GNUC__)
-	__attribute__((optimize("O3")))
+__attribute__((optimize("O3")))
 #endif
 void lvgl_set_px_cb_3bit(struct _disp_drv_t *disp_drv,
-		uint8_t *buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
-		lv_color_t color, lv_opa_t opa)
+			 uint8_t *buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y,
+			 lv_color_t color, lv_opa_t opa)
 {
-	/* x and y should be absolute co-ordinates and vdb should 100%
+	/* INFO: RATCH: x and y should be absolute co-ordinates and vdb should be 100%
 	 * For this _lv_blend_fill is modified to provide absolute co-ordinates
 	 */
-	/* INFO: Calculation of address is hardcoded for LPM0XX */
+	/* INFO: RATCH: Calculation of address is hardcoded for LPM0XX */
 	uint8_t *buf_xy = (uint8_t *)(buf + ((x >> 1) + 1) +  (y * ((buf_w >> 1) + 2)));
 	uint8_t color_3bit = 0;
 
-	/* INFO: Add thresholding if absolutly required. it adds about
+	/* INFO: RATCH: Add thresholding if absolutely required. It adds about
 	 * 6ms to draw time.
 	 */
 	if (LV_COLOR_GET_R(color)) {
@@ -63,23 +63,15 @@ void lvgl_set_px_cb_3bit(struct _disp_drv_t *disp_drv,
 }
 
 void lvgl_rounder_cb_3bit(struct _disp_drv_t *disp_drv,
-		lv_area_t *area)
+			  lv_area_t *area)
 {
 	struct device *display_dev = (struct device *)disp_drv->user_data;
 	struct display_capabilities cap;
 
 	display_get_capabilities(display_dev, &cap);
 
-	if (cap.screen_info & SCREEN_INFO_ALIGNMENT_RESTRICTED) {
-		if (cap.x_alignment != 0) {
-			/* INFO: Hardcoded for LPM0XX */
-			area->x1 = 0;
-			area->x2 = 175;
-		}
-
-		if (cap.y_alignment != 0) {
-			area->y1 &= ~cap.y_alignment;
-			area->y2 |= cap.y_alignment;
-		}
+	if (cap.screen_info & SCREEN_INFO_X_ALIGNMENT_WIDTH) {
+		area->x1 = 0;
+		area->x2 = cap.x_resolution - 1;
 	}
 }
